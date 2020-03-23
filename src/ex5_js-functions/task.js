7f94a0db002f2273080b984e6fb87036256642e9
fortnoxcode <61293470+fortnoxcode@users.noreply.github.com>
2020-03-23 14:32:56 +0400

    fix: checking for number and style fix

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,18 +1,35 @@
 const calculator = {
   value: 0,
   add(num) {
-    if(num) { calculator.value += num; } return calculator.add;
+    if (typeof num === 'number' && !isNaN(num)) {
+      calculator.value += num;
+    }
+    return calculator.add;
   },
   subtract(num) {
-    if(num) { calculator.value -= num; } return calculator.subtract;
+    if (typeof num === 'number' && !isNaN(num)) {
+      calculator.value -= num;
+    }
+    return calculator.subtract;
   },
   multiply(num) {
-    if(num) { calculator.value *= num; } return calculator.multiply;
+    if (typeof num === 'number' && !isNaN(num)) {
+      calculator.value *= num;
+    }
+    return calculator.multiply;
   },
   divide(num) {
-    if(num) { calculator.value /= num; } return calculator.divide;
+    if (typeof num === 'number' && !isNaN(num)) {
+      calculator.value /= num;
+    }
+    return calculator.divide;
   },
-  getResult() { return calculator.value; },
-  reset() { this.value = 0; return this.value;}
+  getResult() {
+    return calculator.value;
+  },
+  reset() {
+    this.value = 0;
+    return this.value;
+  }
 }
 module.exports = calculator;
