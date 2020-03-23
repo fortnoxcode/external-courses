a6a20922a0cb991438c4e9e036bd87ee64d9bb81
fortnoxcode <61293470+fortnoxcode@users.noreply.github.com>
2020-03-23 19:16:12 +0400

    add: task-06 solution

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,7 +1,6 @@
 function reduceAnalog(array, callback, initialValue) {
   let previousValue = initialValue || array[0];
-  let i = initialValue ? 0 : 1;
-  for (; i < array.length; i++) {
+  for (let i = initialValue ? 0 : 1; i < array.length; i++) {
     previousValue = callback(previousValue, array[i], i, array);
   }
   return previousValue;
