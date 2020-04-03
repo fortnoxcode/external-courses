d9069ee0e376041bda153d066c3a0792a651db94
fortnoxcode <61293470+fortnoxcode@users.noreply.github.com>
2020-04-03 12:29:15 +0400

    fix: task-01

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,8 +1,10 @@
 function sliceAnalog(array, begin = 0, end = array.length) {
   let newArr = [];
-  for (let i = begin < 0 ? array.length + begin : begin;
-    i < (end < 0 ? array.length + end : end); i++) {
-    newArr.push(array[i]);
+  let start = begin < 0 ? array.length + begin : begin;
+  let to = end < 0 ? array.length + end : end;
+
+  for (start; start < to; start++) {
+    newArr.push(array[start]);
   }
   return newArr;
 }
